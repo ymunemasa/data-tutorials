527d5cffa5f5762eb8e816ad588d8e475636adf8
mushtaqrizvihw <mrizvi@hortonworks.com>
2016-10-20 16:18:42 -0700

    added some comments

--- tutorials/hortonworks/learning-the-ropes-of-the-hortonworks-sandbox/Foodmart_Data/load_foodmart_data.sh
+++ tutorials/hortonworks/learning-the-ropes-of-the-hortonworks-sandbox/Foodmart_Data/load_foodmart_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Downloading data files"
 cd ~
 wget https://raw.githubusercontent.com/hortonworks/tutorials/hdp-2.5/tutorials/hortonworks/learning-the-ropes-of-the-hortonworks-sandbox/Foodmart_Data/foodmart_data.zip
 wget https://raw.githubusercontent.com/hortonworks/tutorials/hdp-2.5/tutorials/hortonworks/learning-the-ropes-of-the-hortonworks-sandbox/Foodmart_Data/create_table_script.hql
@@ -10,7 +11,7 @@ sudo chown hive /home/hive/create_table_script.hql
 sudo chown hive /home/hive/foodmart_data.zip
 sudo -u hive unzip /home/hive/foodmart_data.zip -d /home/hive
 
-# creating HDFS directories for foodamrt tables
+echo "Creating HDFS directories for Hive external tables"
 sudo -u hive hdfs dfs -mkdir /apps/hive/warehouse/foodmart.db
 sudo -u hive hdfs dfs -mkdir /apps/hive/warehouse/foodmart.db/product
 sudo -u hive hdfs dfs -mkdir /apps/hive/warehouse/foodmart.db/store
@@ -18,7 +19,7 @@ sudo -u hive hdfs dfs -mkdir /apps/hive/warehouse/foodmart.db/customer
 sudo -u hive hdfs dfs -mkdir /apps/hive/warehouse/foodmart.db/sales_fact_1998
 sudo -u hive hdfs dfs -mkdir /apps/hive/warehouse/foodmart.db/inventory_fact_1998
 
-# copying sample data files to HDFS corresponding directories 
+echo "Copying sample data files to HDFS corresponding directories" 
 sudo -u hive hdfs dfs -copyFromLocal /home/hive/foodmart_data/product /apps/hive/warehouse/foodmart.db/product/
 sudo -u hive hdfs dfs -copyFromLocal /home/hive/foodmart_data/store /apps/hive/warehouse/foodmart.db/store/
 sudo -u hive hdfs dfs -copyFromLocal /home/hive/foodmart_data/customer /apps/hive/warehouse/foodmart.db/customer/
@@ -26,4 +27,5 @@ sudo -u hive hdfs dfs -copyFromLocal /home/hive/foodmart_data/sales_fact_1998 /a
 sudo -u hive hdfs dfs -copyFromLocal /home/hive/foodmart_data/inventory_fact_1998 /apps/hive/warehouse/foodmart.db/inventory_fact_1998/
 sudo -u hive hdfs dfs -chmod -R 777 /apps/hive/warehouse/foodmart.db
 
+echo "Creating Hive Tables"
 sudo -u hive hive -f /home/hive/create_table_script.hql
