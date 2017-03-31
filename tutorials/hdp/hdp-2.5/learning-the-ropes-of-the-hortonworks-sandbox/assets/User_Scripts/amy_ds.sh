63f2b92c2483d5aeb711ad3a10462b36b45e31f9
James <jmedel@hortonworks.com>
2017-03-30 18:02:23 -0700

    update amy_ds.sh
    added $CLUSTER_NAME
    $AMBARI_HOST
    
    Commented out Ambari Views on line 21 and 22

--- tutorials/hdp/hdp-2.5/learning-the-ropes-of-the-hortonworks-sandbox/assets/User_Scripts/amy_ds.sh
+++ tutorials/hdp/hdp-2.5/learning-the-ropes-of-the-hortonworks-sandbox/assets/User_Scripts/amy_ds.sh
@@ -10,14 +10,16 @@ echo $USERNAME:$PASSWORD | sudo chpasswd
 echo "Creating a HDFS directory"
 sudo -u hdfs hdfs dfs -mkdir /user/amy_ds
 sudo -u hdfs hdfs dfs -chown -R amy_ds:hdfs /user/amy_ds
+AMBARI_HOST=$(hostname -f)
+CLUSTER_NAME=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters |grep cluster_name|grep -Po ': "(.+)'|grep -Po '[a-zA-Z0-9\-_!?.]+')
 echo "Creating a user in ambari"
-curl -iv -u admin:admin -H "X-Requested-By: ambari" -X POST -d '{"Users/user_name":"amy_ds","Users/password":"amy_ds","Users/active":"true","Users/admin":"false"}' http://sandbox.hortonworks.com:8080/api/v1/users
+curl -iv -u admin:admin -H "X-Requested-By: ambari" -X POST -d '{"Users/user_name":"amy_ds","Users/password":"amy_ds","Users/active":"true","Users/admin":"false"}' http://$AMBARI_HOST:8080/api/v1/users
 echo "Assigning the user to a group in ambari"
-curl -iv -u admin:admin -H "X-Requested-By: ambari"-X POST -d '[{"MemberInfo/user_name":"amy_ds","MemberInfo/group_name":"views"}]' http://sandbox.hortonworks.com:8080/api/v1/groups/views/members
+curl -iv -u admin:admin -H "X-Requested-By: ambari"-X POST -d '[{"MemberInfo/user_name":"amy_ds","MemberInfo/group_name":"views"}]' http://$AMBARI_HOST:8080/api/v1/groups/views/members
 echo "Assigning user to a Sandbox role Service Operator"
-curl -iv -u admin:admin -H "X-Requested-By: ambari" -X POST -d '[{"PrivilegeInfo":{"permission_name":"SERVICE.OPERATOR", "principal_name":"amy_ds","principal_type":"USER"}}]' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/privileges
-echo "Assigning Ambari Views"
-curl -iv -u admin:admin -H  "X-Requested-By: ambari" -X POST -d '[{"PrivilegeInfo":{"permission_name":"VIEW.USER", "principal_name":"amy_ds","principal_type":"USER"}}]' http://127.0.0.1:8080/api/v1/views/HIVE/versions/1.5.0/instances/AUTO_HIVE_INSTANCE/privileges/
+curl -iv -u admin:admin -H "X-Requested-By: ambari" -X POST -d '[{"PrivilegeInfo":{"permission_name":"SERVICE.OPERATOR", "principal_name":"amy_ds","principal_type":"USER"}}]' http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/privileges
+#echo "Assigning Ambari Views"
+#curl -iv -u admin:admin -H  "X-Requested-By: ambari" -X POST -d '[{"PrivilegeInfo":{"permission_name":"VIEW.USER", "principal_name":"amy_ds","principal_type":"USER"}}]' http://127.0.0.1:8080/api/v1/views/HIVE/versions/1.5.0/instances/AUTO_HIVE_INSTANCE/privileges/
 #Get Pig view versions
 pig_version=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/views/PIG/versions | jq -r '.items[].ViewVersionInfo.version')
 #Get Pig View instance name
