51de86fad973991001283db3ffab64c1abbde251
James Medel <jamesmedel94@gmail.com>
2017-04-04 23:46:11 -0700

    added auto_scripts.zip folder
    - currently auto scripts are supported on docker HDF sandbox (support
    pending for VMware and Virtualbox)
    - docker_sandbox_hdf.sh starts the docker container named sandbox-hdf
    and all ambari services on HDF
    - launch_nifi_ui.sh runs the docker_sandbox_hdf.sh script and then
    launches NiFi UI via Google Chrome (planning to add support for
    firefox, safari).

--- tutorials/hdf/hdf-2.1/analyze-traffic-pattern-with-apache-nifi/assets/auto_scripts/docker_sandbox_hdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Start Docker HDF Sandbox..."
-docker start sandbox-hdf
-echo "Starting Ambari Services, may take 2.5 minutes..."
-docker exec -d sandbox-hdf /root/start_sandbox.sh
-sleep 150
--- tutorials/hdf/hdf-2.1/analyze-traffic-pattern-with-apache-nifi/assets/auto_scripts/launch_nifi_ui.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./docker_sandbox_hdf.sh
-#Access NiFi HTML UI via Mac CLI
-echo "Launch NiFi HTML UI..."
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk http://sandbox.hortonworks.com:19090/nifi/
